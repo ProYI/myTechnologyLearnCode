da669f494f85d998164d66e45efe51a0e2c3cc2c
pengchen <chen.peng@nx-engine.com>
2022-04-27 14:03:21 +0800

    add:web-example

---
+++ codeSegment/src/main/java/online/proyi/codeSegment/kotlin/Demo.kt
@@ -0,0 +1,22 @@
+package org.example.kotlin
+
+import java.time.LocalDate
+
+fun main() {
+    var a = 1
+    val s1 = "a is $a"
+
+    println(s1)
+
+    a = 2
+    val s2 = "${s1.replace("is", "was")}, but now is $a"
+    println(s2)
+
+    testLocalDate();
+}
+
+fun testLocalDate() {
+    val localDate = LocalDate.now();
+    println(localDate)
+
+}
